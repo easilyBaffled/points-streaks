632ca3fb2133513b074c1145f46bcc3c8983ab26
Danny Michaelis <dmichaelis0@gmail.com>
2021-12-08 09:31:10 -0500

    infrastructure

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import istanbul from 'vite-plugin-istanbul';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
+// console.tap = (v, ...rest) => (console.log(v, ...rest), v);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-	  react(),
-	  istanbul({
-		  include: 'src/*',
-		  exclude: ['node_modules', 'test/'],
-		  extension: [ '.js', '.ts', '.vue' ],
-		  requireEnv: true,
-	  }),
-  ]
-})
+    react(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "test/"],
+      extension: [".js", ".ts", ".vue"],
+      requireEnv: true,
+    }),
+  ],
+});
