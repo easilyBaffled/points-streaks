374dd4e54efff94909f20842431cccb290cbec31
Danny Michaelis <dmichaelis0@gmail.com>
2021-12-13 10:53:08 -0500

    can read and write to fb

--- src/libs/firebaseConnection.js
+++ src/libs/firebaseConnection.js
@@ -7,15 +7,15 @@
  * [Get | JavaScript SDK  |  Firebase](https://firebase.google.com/docs/reference/js/firebase.database.Reference?authuser=0#get)
  */
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDoc, doc } from "firebase/firestore";
+import { getFirestore, doc } from "firebase/firestore";
 
 import { createFBStorageAPI } from "./createFBStorageAPI";
 
-export async function setupFirebaseConnection(firebaseConfig) {
+export function setupFirebaseConnection(firebaseConfig) {
     const firebaseApp = initializeApp(firebaseConfig);
     const db = getFirestore();
 
-    return getDoc(doc(db, "state", "state")).catch(console.error); // getDocs(collection(db, "state")).catch(console.error);
+    return doc(db, "state", "state");
 }
 
 export const initRealtimeFirebaseDB = (firebaseConfig, throttleTime) => {
