963f8b980b50b3a25d485dbb598e70439ce89e18
Danny Michaelis <dmichaelis0@gmail.com>
2021-12-30 11:01:10 -0500

    add "simple" PWA setup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import istanbul from "vite-plugin-istanbul";
+import { VitePWA } from "vite-plugin-pwa";
 
 // console.tap = (v, ...rest) => (console.log(v, ...rest), v);
 
@@ -13,7 +14,8 @@ export default defineConfig({
             exclude:   [ "node_modules", "test/" ],
             extension: [ ".js", ".jsx" ],
             include:   "src/*"
-        })
+        }),
+        VitePWA({})
     ],
     resolve: {
         alias: {
