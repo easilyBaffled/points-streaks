6f2531801dc7dc9a3406b33bcfb619ec488a8b11
Dan Michaelis <dmichaelis0+github@gmail.com>
2022-01-10 18:26:48 -0500

    links open new tab

--- src/components/Task/DefaultName.jsx
+++ src/components/Task/DefaultName.jsx
@@ -2,8 +2,19 @@ import clsx from "clsx";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
+function LinkRenderer( props ) {
+    return (
+        <a href={props.href} target="_blank" rel="noreferrer">
+            {props.children}
+        </a>
+    );
+}
+
 export const DefaultName = ({ task, children, isDone }) => (
-    <ReactMarkdown className={clsx( "name", { done: isDone })}>
+    <ReactMarkdown
+        components={{ a: LinkRenderer }}
+        className={clsx( "name", { done: isDone })}
+    >
         {children ?? task}
     </ReactMarkdown>
 );
