7a916b43441cdb906f2045a7e4828fb0eb0aecb1
Dan Michaelis <dmichaelis0+github@gmail.com>
2022-02-03 16:34:22 -0500

    add download to backlog

--- src/features/backlog/components/backlogPage.jsx
+++ src/features/backlog/components/backlogPage.jsx
@@ -26,6 +26,18 @@ export const _BacklogPage = ({ backlogTasks }) => {
                 className="focus:ring-indigo-500 focus:border-indigo-500 pl-3 sm:text-sm border-2 border-gray-300 rounded-md"
                 onChange={( e ) => setFilter( e.target.value )}
             />
+            <a
+                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                download="backlog.txt"
+                href={
+                    "data:text/plain;charset=utf-8," +
+                    encodeURIComponent(
+                        backlogTasks.map( ( task ) => task.fullTask ).join( "\n" )
+                    )
+                }
+            >
+                Download List as Text
+            </a>
             <code>{backlogTasks.length}</code>
             {backlogTasks.length && (
                 <>
